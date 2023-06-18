5250e75fc44fd013bf43bde3b8d55dc286fb372c
romanovULSTU <99944605+romanovULSTU@users.noreply.github.com>
2023-06-18 04:07:10 +0400

    Описание изменений

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+module.exports = {
+    mode: "development",
+    entry:"./src/Index.js",
+    output: {
+        filename : "main.js",
+        path: path.resolve(__dirname,"dist"),
+    },
+    devServer:{
+        static:{
+            directory:path.join(__dirname,"dist"),
+        },
+        compress:true,
+        port:9000,
+    },
+};
